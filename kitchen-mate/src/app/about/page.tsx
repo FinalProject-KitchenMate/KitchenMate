c4167e9012fea283e47f5e7dca99e85783790cc1
ELvan Surahmat <146643689+ElvanSurahmat@users.noreply.github.com>
2024-02-14 15:22:16 +0700

    about

--- kitchen-mate/src/app/about/page.tsx
+++ kitchen-mate/src/app/about/page.tsx
@@ -1,91 +1,318 @@
 "use client";
-import { motion } from "framer-motion";
-export default function AboutPage() {
+import Brain from "@/components/brain";
+import { motion, useInView, useScroll } from "framer-motion";
+import Image from "next/image";
+import { useRef } from "react";
+
+const AboutPage = () => {
+  const containerRef = useRef();
+
+  const { scrollYProgress } = useScroll({ container: containerRef?.current });
+
+  const skillRef = useRef();
+  // const isSkillRefInView = useInView(skillRef, { once: true });
+  const isSkillRefInView = useInView(skillRef, { margin: "-100px" });
+
+  const experienceRef = useRef();
+  const isExperienceRefInView = useInView(experienceRef, { margin: "-100px" });
+
   return (
-    <motion.div className="h-full">
-      <div>
-        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-32 zl:gap-64">
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full overflow-scroll lg:flex" ref={containerRef}>
+        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:w-1/2">
           <div className="flex flex-col gap-12 justify-center">
-            <h1 className="font-bold text-2xl">Biography</h1>
+            <h1 className="font-bold text-2xl">BIOGRAPHY</h1>
+
             <p className="text-lg">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas
-              inventore rerum sint? Recusandae ex, sequi sed quisquam incidunt,
-              ab dicta possimus, reprehenderit rerum itaque eius dolores nostrum
-              vel delectus deserunt?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              harum quibusdam cupiditate nobis accusamus sed aut aperiam,
+              reiciendis numquam! Voluptas voluptatibus obcaecati dolore itaque
+              suscipit! Vel doloremque numquam quam nihil.
             </p>
+
             <span className="italic">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudan
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </span>
+
+            <motion.svg
+              initial={{ opacity: 0.2, y: 0 }}
+              animate={{ opacity: 1, y: "10px" }}
+              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width={50}
+              height={50}
+            >
+              <path
+                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+              <path
+                d="M15 11L12 14L9 11"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+            </motion.svg>
           </div>
-          <div className="flex flex-col gap-12 justify-center">
-            <h1 className="font-bold text-2xl">Skill</h1>
-            <div className="flex gap-4 flex-wrap">
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-slate-400 hover:text-blue-400">
-                JavaSript
+
+          <div className="flex flex-col gap-12 justify-center" ref={skillRef}>
+            <motion.h1
+              initial={{ x: "-300px" }}
+              animate={isSkillRefInView ? { x: 0 } : {}}
+              transition={{ delay: 0.2 }}
+              className="font-bold text-2xl"
+            >
+              Frame Work
+            </motion.h1>
+
+            <motion.div
+              initial={{ x: "-300px" }}
+              animate={isSkillRefInView ? { x: 0 } : {}}
+              className="flex gap-4 flex-wrap"
+            >
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                JavaScript
               </div>
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-slate-400 hover:text-blue-400">
-                HTTP
-              </div>
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-slate-400 hover:text-blue-400">
-                CSS
-              </div>
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-slate-400 hover:text-blue-400">
-                React
-              </div>
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-slate-400 hover:text-blue-400">
-                NEXT.js
-              </div>
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-slate-400 hover:text-blue-400">
-                Expo
-              </div>
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-slate-400 hover:text-blue-400">
-                MongoDB
-              </div>
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-slate-400 hover:text-blue-400">
-                Node.JS
-              </div>
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-slate-400 hover:text-blue-400">
-                Tailwind CSS
-              </div>
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-slate-400 hover:text-blue-400">
-                GraphQl
-              </div>
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-slate-400 hover:text-blue-400">
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
                 TypeScript
               </div>
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-slate-400 hover:text-blue-400">
-                PostgresSQL
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                React.js
               </div>
-            </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Next.js
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                SCSS
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Tailwind CSS
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                MongoDB
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                PostgreSQL
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Node.js
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Nest.js
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Express.js
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Spring Boot
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                GraphQL
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Apollo
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Redux
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Framer Motion
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Three.js
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                WebGL
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Webpack
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Vite
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Docker
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                AWS
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Firebase
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Git
+              </div>
+              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Figma
+              </div>
+            </motion.div>
+
+            <motion.svg
+              initial={{ opacity: 0.2, y: 0 }}
+              animate={{ opacity: 1, y: "10px" }}
+              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width={50}
+              height={50}
+            >
+              <path
+                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+              <path
+                d="M15 11L12 14L9 11"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+            </motion.svg>
           </div>
-          <div className="flex flex-col gap-12 justify-center pb-48">
-            <h1 className="font-bold text-2xl">Experient</h1>
-            <div className="">
-              {/*experient list item*/}
+
+          <div
+            className="flex flex-col gap-12 justify-center pb-48"
+            ref={experienceRef}
+          >
+            <motion.h1
+              initial={{ x: "-300px" }}
+              animate={isExperienceRefInView ? { x: "0" } : {}}
+              transition={{ delay: 0.2 }}
+              className="font-bold text-2xl"
+            >
+              Team
+            </motion.h1>
+
+            <motion.div
+              initial={{ x: "-300px" }}
+              animate={isExperienceRefInView ? { x: "0" } : {}}
+              className=""
+            >
               <div className="flex justify-between h-48">
-                {/*Left*/}
-                <div className="w-1/3">
-                  <div className="">Title</div>
-                  <div className="">description</div>
-                  <div className="">Date</div>
-                </div>
-                {/*Center*/}
-                <div className="w-1/6">
-                  {/*Line*/}
-                  <div className="">
-                    {/*Circle*/}
-                    <div className=""></div>
+                <div className="w-1/3 ">
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Elvan Surahmat
+                  </div>
+
+                  <div className="p-3 text-sm italic">
+                    "Membuat pengalaman online yang menarik dan mudah diakses
+                    untuk Anda."{" "}
+                  </div>
+
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    Front-End
+                  </div>
+
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    RMT-44
                   </div>
                 </div>
-                {/*Right*/}
-                <div>
-                  <div className="w-1/3"></div>
+
+                <div className="w-1/6 flex justify-center">
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+
+                <div className="w-1/3 "></div>
+              </div>
+
+              <div className="flex justify-between h-48">
+                <div className="w-1/3 "></div>
+
+                <div className="w-1/6 flex justify-center">
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+
+                <div className="w-1/3 ">
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Elvan Surahmat
+                  </div>
+
+                  <div className="p-3 text-sm italic">
+                    "Membuat pengalaman online yang menarik dan mudah diakses{" "}
+                  </div>
+
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    Front-End{" "}
+                  </div>
+
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    RMT-44
+                  </div>
                 </div>
               </div>
-            </div>
+
+              <div className="flex justify-between h-48">
+                <div className="w-1/3 ">
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Freelancer{" "}
+                  </div>
+
+                  <div className="p-3 text-sm italic">
+                    I provided web solutions, applying a range of technologies
+                    to address client requirements.{" "}
+                  </div>
+
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2010 - 2019{" "}
+                  </div>
+                </div>
+
+                <div className="w-1/6 flex justify-center">
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+
+                <div className="w-1/3 "></div>
+              </div>
+              <div className="flex justify-between h-48">
+                <div className="w-1/3 "></div>
+
+                <div className="w-1/6 flex justify-center">
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+
+                <div className="w-1/3 ">
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Elvan Surahmat
+                  </div>
+
+                  <div className="p-3 text-sm italic">untuk Anda." </div>
+
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    Front-End{" "}
+                  </div>
+
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    RMT-44
+                  </div>
+                </div>
+              </div>
+            </motion.div>
           </div>
+        </div>
+        {/* SVG CONTAINER */}
+        <div className="hidden lg:block w-1/3 sticky top-0 z-30 xl:w-1/2">
+          <Brain scrollYProgress={scrollYProgress} />
         </div>
       </div>
     </motion.div>
   );
-}
+};
+
+export default AboutPage;
