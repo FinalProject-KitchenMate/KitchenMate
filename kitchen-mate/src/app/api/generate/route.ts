21c2ccd27b71bffb22914b32b3da8d624f1467b5
Reinawan <admin@reinawan.com>
2024-02-15 13:30:11 +0700

    feat: server-generate

--- kitchen-mate/src/app/api/generate/route.ts
+++ kitchen-mate/src/app/api/generate/route.ts
@@ -1,66 +1,33 @@
-import { fetchOpenAICompletionsStream } from '@/db/models/generate';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from "next/server";
+import axios from 'axios';
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(request: NextRequest) {
   try {
-      console.log('Request Query:<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<', req.query);
-      const ingredients = req.query.ingredients as string;
-      const mealType = req.query.mealType as string;
-      const cuisine = req.query.cuisine as string;
-      const cookingTime = req.query.cookingTime as string;
-      const complexity = req.query.complexity as string;
+    const body = await request.json();
+console.log(body,"prompt>>>>>>>>");
 
-
-      res.setHeader('Content-Type', 'text/event-stream');
-      res.setHeader('Cache-Control', 'no-cache');
-      res.setHeader('Connection', 'keep-alive');
-
-
-      const sendEvent = (chunk: any) => {
-        let chunkResponse;
-        if (chunk.choices[0].finish_reason === 'stop') {
-          res.write(`data: ${JSON.stringify({ action: 'close' })}\n\n`);
-        } else {
-          if (chunk.choices[0].delta.role && chunk.choices[0].delta.role === 'assistant') {
-            chunkResponse = {
-              action: 'start',
-            };
-          } else {
-            chunkResponse = {
-              action: 'chunk',
-              chunk: chunk.choices[0].delta.content,
-            };
-          }
-          res.write(`data: ${JSON.stringify(chunkResponse)}\n\n`);
-        }
-      };
-
-      const prompt = [];
-      prompt.push('Generate a recipe that incorporates the following details:');
-      prompt.push(`[Ingredients: ${ingredients}]`);
-      prompt.push(`[Meal Type: ${mealType}]`);
-      prompt.push(`[Cuisine Preference: ${cuisine}]`);
-      prompt.push(`[Cooking Time: ${cookingTime}]`);
-      prompt.push(`[Complexity: ${complexity}]`);
-      prompt.push(
-        'Please provide a detailed recipe, including steps for preparation and cooking. Only use the ingredients provided.'
-      );
-      prompt.push('The recipe should highlight the fresh and vibrant flavors of the ingredients.');
-      prompt.push('Also give the recipe a suitable name in its local language based on cuisine preference.');
-
-      const messages = [
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', {      
+      model: 'gpt-3.5-turbo',
+      messages: [
         {
-          role: 'system',
-          content: prompt.join(' '),
-        },
-      ];
-      await fetchOpenAICompletionsStream(messages, sendEvent);
+          "role": "user",
+          "content": body.messages
+        }
+      ] ,
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+      }
+    });    
+console.log(response,"response>>>>>>>>");
 
-      req.on('close', () => {
-        res.end();
-      });
+    return NextResponse.json({ text: response.data.choices[0].message.content });
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.log(error,"error<<<<<");
+    
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
+
+
