48d055c62ea9a658d4407f3e2be78422a83fe7e3
ELvan Surahmat <146643689+ElvanSurahmat@users.noreply.github.com>
2024-02-15 16:18:53 +0700

    fetch home

---
+++ kitchen-mate/src/components/Card.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import React from "react";
+export interface Result {
+  id: number;
+  title: string;
+  image: string;
+  imageType: string;
+}
+
+const Card = ({ recipt }: { recipt: Result }) => {
+  return (
+    <Link href={`/resep/${recipt.id}`}>
+      <div className="relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+        <div className="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white shadow-lg bg-clip-border rounded-xl h-80">
+          <img src={recipt.image} alt="profile-picture" />
+        </div>
+        <div className="p-6 text-center">
+          <h4 className="block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+            {recipt.title}
+          </h4>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
