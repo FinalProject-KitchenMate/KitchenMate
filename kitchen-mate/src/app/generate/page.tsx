b130dba2e0e14584f1a88318d4fadcd9309cc128
Reinawan <admin@reinawan.com>
2024-02-16 10:20:11 +0700

    fix: client-generate1

--- kitchen-mate/src/app/generate/page.tsx
+++ kitchen-mate/src/app/generate/page.tsx
@@ -1,15 +1,26 @@
-'use client';
+'use client'
 import { useState } from 'react';
 import axios from 'axios';
 
 const Home = () => {
-  const [inputText, setInputText] = useState('');
+  const [promtIngredients] = useState('make a recipe based on the ingredients');
+  const [ingredients, setIngredients] = useState('');
+  const [promtMealType] = useState('and meal type');
+  const [mealType, setMealType] = useState('Breakfast');
+  const [promtCookingTime] = useState(',cooking time for');
+  const [cookingTime, setCookingTime] = useState('Less than 5 minutes');
+  const [promtFind] = useState(', and make a name for the title of the recipe');
   const [outputText, setOutputText] = useState('');
+  console.log(promtIngredients, ingredients, promtMealType, mealType, promtCookingTime, cookingTime, promtFind);
+
+
 
   const handleSubmit = async () => {
     try {
+      const combinedInput = `${promtIngredients.trim()}, ${ingredients.trim()}, ${promtMealType.trim()}, ${mealType.trim()}, ${promtCookingTime.trim()}, ${cookingTime.trim()}, ${promtFind.trim()}`;
+
       const response = await axios.post(process.env.NEXT_PUBLIC_BASE_URL + '/api/generate', {
-        messages: inputText.trim()
+        messages: combinedInput
       });
 
       setOutputText(response.data.text);
@@ -19,21 +30,78 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <h1>Chat with GPT-3.5 Turbo</h1>
-      <div>
-        <textarea value={inputText} onChange={(e) => setInputText(e.target.value)} rows={5} cols={50} />
-      </div>
-      <div>
-        <button onClick={handleSubmit}>Submit</button>
-      </div>
-      {outputText && (
-        <div>
-          <h2>Response:</h2>
-          <p>{outputText}</p>
+    <>
+      <aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+        <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+          <ul className="space-y-2 font-medium mt-4">
+            <li>
+              <div>
+                <h1>Generate Your Recipe</h1>
+                <div>
+                  <label className="form-control w-full max-w-xs mb-4 mt-4">
+                    <div className="label">
+                      <span className="label-text"><b>Ingredients</b></span>
+                    </div>
+                    <input
+                      type="text"
+                      placeholder="rice, egg, onion"
+                      className="input input-bordered w-full max-w-xs"
+                      value={ingredients} onChange={(e) => setIngredients(e.target.value)}
+                    />
+                  </label>
+
+                  <label className="form-control w-full max-w-xs mb-4">
+                    <div className="label">
+                      <span className="label-text"><b>Meal Type</b></span>
+                    </div>
+                    <select className="select select-bordered" value={mealType} onChange={(e) => setMealType(e.target.value)}>
+                      <option>Breakfast</option>
+                      <option>Lunch</option>
+                      <option>Dinner</option>
+                      <option>Snack</option>
+                    </select>
+                  </label>
+
+                  <label className="form-control w-full max-w-xs mb-4">
+                    <div className="label">
+                      <span className="label-text"><b>Cooking Time</b></span>
+                    </div>
+                    <select className="select select-bordered" value={cookingTime} onChange={(e) => setCookingTime(e.target.value)}>
+                      <option>Less than 30 minutes</option>
+                      <option>30-60 minutes</option>
+                      <option>More than 1 hour</option>
+                    </select>
+                  </label>
+                </div>
+                <div>
+                  <button className="btn btn-outline btn-primary" onClick={handleSubmit}>Submit</button>
+                </div>
+                {/* {outputText && (
+                  <div>
+                    <h2>Response:</h2>
+                    <p>{outputText}</p>
+                  </div>
+                )} */}
+
+              </div>
+            </li>
+          </ul>
         </div>
-      )}
-    </div>
+      </aside>
+
+      <div className="p-4 sm:ml-64">
+        <h1 className='text-xl '>Generate Your Recipe</h1>
+        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-7">
+          {outputText && (
+            <div>
+              <h2>Response:</h2>
+              <p>{outputText}</p>
+            </div>
+          )}
+
+        </div>
+      </div>
+    </>
   );
 };
 
