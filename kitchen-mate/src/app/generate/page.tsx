2abb8317513e1fa8e7422c8d59a27923c4947a71
rifqiabil <abilrifqi@gmail.com>
2024-02-21 03:27:10 +0700

    client-my-recipe

--- kitchen-mate/src/app/generate/page.tsx
+++ kitchen-mate/src/app/generate/page.tsx
@@ -206,11 +206,11 @@ const GeneratePage = () => {
                     <ul>
                       <li className="text-gray-600 dark:text-gray-400">
                         <b>Ready in: </b>
-                        {outputJSON.readyInMinutes} minutes
+                        {outputJSON.readyInMinutes} Minutes
                       </li>
                       <li className="text-gray-600 dark:text-gray-400">
                         <b>Servings: </b>
-                        {outputJSON.servings}
+                        {outputJSON.servings} Servings
                       </li>
                       <li className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-5">
                         Ingredients :
