a4a505a6290befd233a98f45e3287956e0059d68
rifqiabil <abilrifqi@gmail.com>
2024-02-21 13:49:05 +0700

    fix auth

--- kitchen-mate/src/app/resep/page.tsx
+++ kitchen-mate/src/app/resep/page.tsx
@@ -55,7 +55,7 @@ export default function Resep() {
 
   return (
     <>
-      <ServerProtectedComponent>
+      {/* <ServerProtectedComponent> */}
         <aside
           id="logo-sidebar"
           className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full  border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
@@ -134,7 +134,7 @@ export default function Resep() {
             </div>
           </div>
         </div>
-      </ServerProtectedComponent>
+      {/* </ServerProtectedComponent> */}
     </>
   );
 }
